3fa214ab77d87f621e30b8cfb91a751797d29e9f
mike-lowski <thisisnotanotheremail@protonmail.com>
2024-10-05 14:59:24 +0200

    textfunk

--- main.ts
+++ main.ts
@@ -1,134 +1,124 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class JoinLinesPlugin extends Plugin {
+  async onload() {
+    // Command to join lines
+    this.addCommand({
+      id: "join-lines",
+      name: "Join lines",
+      editorCallback: (editor: Editor) => {
+        joinLines(editor);
+      },
+    });
 
-interface MyPluginSettings {
-	mySetting: string;
+    // Command to remove empty lines
+    this.addCommand({
+      id: "remove-empty-lines",
+      name: "Remove Empty Lines",
+      editorCallback: (editor: Editor) => {
+        removeEmptyLines(editor);
+      },
+    });
+
+    // Command to remove consecutive empty lines
+    this.addCommand({
+      id: "remove-consecutive-empty-lines",
+      name: "Remove Consecutive Empty Lines",
+      editorCallback: (editor: Editor) => {
+        removeConsecutiveEmptyLines(editor);
+      },
+    });
+
+    // Command to trim leading and trailing spaces
+    this.addCommand({
+      id: "trim-leading-trailing-spaces",
+      name: "Trim Leading and Trailing Spaces",
+      editorCallback: (editor: Editor) => {
+        trimLeadingTrailingSpaces(editor);
+      },
+    });
+  }
+
+  onunload() {
+    // Clean up resources if needed
+  }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+// Function to join lines (only works on selected text)
+function joinLines(editor: Editor) {
+  const selectedText = editor.getSelection();
+  if (!selectedText) return;
+
+  const joinedText = selectedText.replace(/\n+/g, " ").replace(/\s+/g, " ");
+  editor.replaceSelection(joinedText);
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// Function to remove empty lines
+function removeEmptyLines(editor: Editor) {
+  const selectedText = editor.getSelection();
+  
+  if (selectedText) {
+    const cleanedText = selectedText
+      .split("\n")
+      .filter(line => line.trim() !== "")
+      .join("\n");
 
-	async onload() {
-		await this.loadSettings();
+    editor.replaceSelection(cleanedText);
+  } else {
+    const entireText = editor.getValue();
+    const cleanedText = entireText
+      .split("\n")
+      .filter(line => line.trim() !== "")
+      .join("\n");
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    editor.setValue(cleanedText);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// Function to remove consecutive empty lines
+function removeConsecutiveEmptyLines(editor: Editor) {
+  const selectedText = editor.getSelection();
+  
+  if (selectedText) {
+    const cleanedText = selectedText
+      .split("\n")
+      .filter((line, index, lines) => 
+        !(line.trim() === "" && lines[index - 1]?.trim() === "")
+      )
+      .join("\n");
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    editor.replaceSelection(cleanedText);
+  } else {
+    const entireText = editor.getValue();
+    const cleanedText = entireText
+      .split("\n")
+      .filter((line, index, lines) => 
+        !(line.trim() === "" && lines[index - 1]?.trim() === "")
+      )
+      .join("\n");
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    editor.setValue(cleanedText);
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+// Function to trim leading and trailing spaces
+function trimLeadingTrailingSpaces(editor: Editor) {
+  const selectedText = editor.getSelection();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  if (selectedText) {
+    const trimmedText = selectedText
+      .split("\n")
+      .map(line => line.trim())
+      .join("\n");
 
-	display(): void {
-		const {containerEl} = this;
+    editor.replaceSelection(trimmedText);
+  } else {
+    const entireText = editor.getValue();
+    const trimmedText = entireText
+      .split("\n")
+      .map(line => line.trim())
+      .join("\n");
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    editor.setValue(trimmedText);
+  }
 }
